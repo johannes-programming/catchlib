0fae84a9b8b9be7b9c21afc664e2a3aeefe06654
Johannes <johannes.programming@gmail.com>
2025-09-11 22:47:59 +0200

    Version 1.0.1

--- src/catchlib/core/__init__.py
+++ src/catchlib/core/__init__.py
@@ -1,6 +1,8 @@
 from contextlib import contextmanager
 from typing import *
 
+from overloadable import Overloadable
+
 __all__ = ["Catcher"]
 
 
@@ -30,3 +32,24 @@ class Catcher:
     def caught(self: Self) -> Optional[BaseException]:
         "This property stores the caught exception."
         return self._caught
+
+    @Overloadable
+    def release(self: Self, *args: Any, **kwargs: Any) -> bool:
+        "This method raises the caught exception."
+        return bool(len(args) + len(kwargs))
+
+    @release.overload(False)
+    def release(self: Self) -> None:
+        "This method raises the caught exception."
+        exc: BaseException = self.caught
+        self._caught = None
+        if exc is not None:
+            raise exc
+
+    @release.overload(True)
+    def release(self: Self, cause: Optional[BaseException]) -> None:
+        "This method raises the caught exception."
+        exc: BaseException = self.caught
+        self._caught = None
+        if exc is not None:
+            raise exc from cause
